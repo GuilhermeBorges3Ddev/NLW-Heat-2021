9409c8a1101b3576519d47f47cdb4159d2660916
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2021-10-25 00:26:54 -0300

    Websocket created with socket.io

---
+++ src/services/GetLast3MessagesService.ts
@@ -0,0 +1,9 @@
+import { io } from "../app";
+import prismaClient from "../prisma";
+
+class GetLast3MessagesService {
+    async execute(text: string, user_id: string) {
+    }
+}
+
+export { GetLast3MessagesService }
